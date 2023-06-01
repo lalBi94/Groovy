f1226c5bac50d61fb30edf90c903f5b2b7c3c183
lalBi94 <bilal@boudjemline.fr>
2023-06-01 04:30:53 +0200

    $

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from './App'
 import "./style/Core.scss"
 import { BrowserRouter } from 'react-router-dom'
 import "./assets/fontawesome"
+import proxyConfig from './proxy.js';
+
+proxyConfig(App)
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root')
